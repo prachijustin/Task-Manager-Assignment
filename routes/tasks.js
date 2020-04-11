e1733a25544081250bff981d71ea8f219bbafde5
prachijustin <prachi.sharma01sep@gmail.com>
2020-04-11 13:00:45 +0530

    API to update and delete task added

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,6 +8,7 @@ route.get('/', async (req, res) => {
     res.status(200).send(tasks)
 })
 
+
 route.post('/', async (req, res) => {
     const newTask = await Tasks.create(
                         {   title: req.body.title,
@@ -18,6 +19,7 @@ route.post('/', async (req, res) => {
                         })
     res.status(200).send('Task created successfully!!' + newTask)
 })
+
 
 route.get('/:id', async (req, res, next) => {
     if(isNaN(parseInt(req.params.id))){
@@ -30,6 +32,7 @@ route.get('/:id', async (req, res, next) => {
         res.status(404).send(`Task with Task ID ${req.params.id} not found.`)
 })
 
+
 route.get('/:id/notes', async (req, res) => {
     if(isNaN(parseInt(req.params.id))){
         res.status(500).send('Task ID should be a number')
@@ -41,7 +44,8 @@ route.get('/:id/notes', async (req, res) => {
         res.status(404).send(`Notes for Task ID ${req.params.id} not found.`)
 })
 
-route.post('/:id/notes', async (req, res, next) => {
+
+route.post('/:id/notes', async (req, res) => {
     if(isNaN(parseInt(req.params.id))){
         res.status(500).send('Task ID should be a number')
     }
@@ -51,5 +55,56 @@ route.post('/:id/notes', async (req, res, next) => {
                 }) 
     res.status(200).send('Note added successfully!!' + newNote)   
 })
+
+
+route.patch('/:id', async (req, res) => {
+    if(isNaN(parseInt(req.params.id))){
+        res.status(500).send('Task ID should be a number')
+    }
+    const task = await Tasks.findByPk(req.params.id)
+    if(task != null){
+        const toUpdate = ['dueDate', 'status', 'priority']
+        const isValid = Object.keys(req.body).every(key => toUpdate.includes(key))
+
+        if(isValid){
+            const updatedValues = { dueDate: req.body.dueDate,
+                            status: req.body.status,
+                            priority: req.body.priority }
+            const updatedTask = await Tasks.update(updatedValues, { where: {
+                                    taskID: req.params.id
+                                }});
+            if(updatedTask>0)
+                res.status(200).send('Task updated successfully!!')
+            else
+                res.status(400).send('Error during updating task.')
+        }
+        else
+            res.status(400).send('Cannot update specified values of task.')
+    }       
+    else
+        res.status(404).send(`Task with Task ID ${req.params.id} not found.`)
+})
+
+
+route.delete('/:id', async(req, res) => {
+    if(isNaN(parseInt(req.params.id))){
+        res.status(500).send('Task ID should be a number')
+    }
+    const task = await Tasks.findByPk(req.params.id)
+    if(task!=null){
+        const deletedTask = await Tasks.destroy({
+                                where: {
+                                    taskID: req.params.id
+                                }
+                            })
+        if(deletedTask>0)
+            res.status(200).send('Task deleted successfully!!')
+        else
+            res.status(400).send('Error during deleting task.')
+    }
+    else
+        res.status(404).send(`Task with Task ID ${req.params.id} not found.`)
+})
+
 
 exports = module.exports = route
